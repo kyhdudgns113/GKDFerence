0960ae9526468a2ba8eb014805cb9edb33ecf235
unknown <dudgns113@gmail.com>
2024-08-04 00:08:41 +0900

    24.08.04 문서 선택 및 드래그 코드 리팩토링

--- front/src/pages/DocumentGPage/DocumentGPage.tsx
+++ front/src/pages/DocumentGPage/DocumentGPage.tsx
@@ -1,117 +1,116 @@
-import {ChangeEvent, MouseEvent, useCallback, useEffect, useRef, useState} from 'react'
+import {MouseEvent, useCallback, useEffect, useRef, useState} from 'react'
 import {Button, Title} from '../../components'
 import {useDocumentGContext} from '../../contexts'
 
 export default function DocumentGPage() {
   /* eslint-disable */
   const {title, contents, addInfoToChangeQ, onBlurTitle, onChangeTitle} = useDocumentGContext()
+
+  const [mouseDownRow, setMouseDownRow] = useState<number | null>(null)
+  const [mouseUpRow, setMouseUpRow] = useState<number | null>(null)
+  const [mouseOverRow, setMouseOverRow] = useState<number | null>(null)
   const [isMouseDown, setIsMouseDown] = useState<boolean>(false)
-  const [mouseDownStart, setMouseDownStart] = useState<number>(-1)
-  const [mouseDownEnd, setMouseDownEnd] = useState<number>(-1)
-  const [isSelectContents, setIsSelectContents] = useState<boolean>(false)
+  const [isMouseOutDuringDown, setIsMouseOutDuringDown] = useState<boolean>(false)
 
   // inputRefs : 이걸 써야 blur 가 된다.
   const inputRefs = useRef<(HTMLInputElement | null)[]>([])
   const divRefs = useRef<(HTMLDivElement | null)[]>([])
-
   /* eslint-enable */
 
-  // AREA2: Test button Area
-  const onClickTestButton = useCallback((e: MouseEvent) => {
-    if (divRefs.current.length > 3) {
-      const selection = window.getSelection()
-      const range = document.createRange()
-
-      range.setStart(divRefs.current[1]!, 0)
-      range.setEnd(divRefs.current[3]!, 1)
-
-      selection?.removeAllRanges()
-      selection?.addRange(range)
-    }
-  }, [])
-
-  // AREA1: div's Event Area
-  const divOnMouseDown = useCallback((e: MouseEvent) => {
+  // AREA1: Div Page Area
+  const onMouseDownPage = useCallback((e: MouseEvent) => {
     setIsMouseDown(true)
-    setMouseDownStart(-1)
-    setIsSelectContents(false)
+    setIsMouseOutDuringDown(false)
+    setMouseDownRow(null)
+    setMouseUpRow(null)
   }, [])
-  const divOnMouseUp = useCallback(
-    (e: MouseEvent) => {
-      setIsMouseDown(false)
-      if (isSelectContents) {
-        // FUTURE: 나중에 0 혹은 끝으로 바꿔야 한다.
-        setMouseDownEnd(0)
-      }
-    },
-    [isSelectContents]
-  )
+  const onMouseOverPage = useCallback((e: MouseEvent) => {
+    setMouseOverRow(null)
+  }, [])
+  const onMouseUpPage = useCallback((e: MouseEvent) => {
+    setIsMouseDown(false)
 
-  // AREA2: input_content's event area
-  const inputOnChange = useCallback(
-    (index: number) => (e: ChangeEvent) => {
-      //
+    // NOTE: 이 상태는 MouseDown 된 상태로 MouseOut 된적이 있는지 저장하는 상태다.
+    // NOTE: MouseUp 이 되었다고 해서 그 상태가 사라지는건 아니다.
+    // setIsMouseOutDuringDown(false)
+    setMouseDownRow(null)
+    setMouseUpRow(null)
+  }, [])
+
+  // AREA2: input Element Area
+  const onClickInput = useCallback(
+    (index: number) => (e: MouseEvent) => {
+      // MouseDown -> MouseOut -> MouseEnter -> MouseUp 이어도 실행된다.
+      // console.log('Click')
     },
     []
   )
-  const inputOnMouseDown = useCallback(
+  const onMouseDownInput = useCallback(
     (index: number) => (e: MouseEvent) => {
       e.stopPropagation()
+      // console.log('MouseDown')
       setIsMouseDown(true)
-      setMouseDownStart(index)
-      setIsSelectContents(false)
+      setIsMouseOutDuringDown(false)
+      setMouseDownRow(index)
     },
     []
   )
-  const inputOnMouseUp = useCallback(
-    (index: number) => (e: MouseEvent) => {
-      e.stopPropagation()
-      setIsMouseDown(false)
-      if (isSelectContents) {
-        setMouseDownEnd(index)
-      }
-    },
-    [isSelectContents]
-  )
-  const inputOnMouseOut = useCallback(
+  const onMouseOutInput = useCallback(
     (index: number) => (e: MouseEvent) => {
       if (isMouseDown) {
-        setIsSelectContents(true)
+        setIsMouseOutDuringDown(true)
       }
     },
     [isMouseDown]
   )
-  const inputOnMouseOver = useCallback(
+  const onMouseOverInput = useCallback(
     (index: number) => (e: MouseEvent) => {
-      if (isSelectContents) {
-        setMouseDownEnd(index)
-      }
+      e.stopPropagation()
+      setMouseOverRow(index)
     },
-    [isSelectContents]
+    []
+  )
+  const onMouseUpInput = useCallback(
+    (index: number) => (e: MouseEvent) => {
+      e.stopPropagation()
+      // console.log('MouseUp')
+      setIsMouseDown(false)
+      setMouseUpRow(index)
+    },
+    []
   )
 
   useEffect(() => {
-    if (isSelectContents) {
-      const lowerIndex = Math.min(mouseDownStart, mouseDownEnd)
-      const higherIndex = Math.max(mouseDownStart, mouseDownEnd)
-
+    if (divRefs.current && isMouseOutDuringDown && isMouseDown) {
       const selection = window.getSelection()
+
       if (selection) {
         const range = document.createRange()
-        range.setStart(divRefs.current[lowerIndex]!, 0)
-        range.setEnd(divRefs.current[higherIndex]!, 1)
+
+        if (mouseDownRow !== null && mouseOverRow !== null) {
+          const lowIndex = Math.min(mouseDownRow, mouseOverRow)
+          const highIndex = Math.max(mouseDownRow, mouseOverRow)
+          const lowRef = divRefs.current[lowIndex]
+          const highRef = divRefs.current[highIndex]
+
+          if (lowRef && highRef) {
+            range.setStart(lowRef, 0)
+            range.setEnd(highRef, 1)
+          }
+        }
 
         selection.removeAllRanges()
         selection.addRange(range)
       }
     }
-  }, [mouseDownStart, mouseDownEnd, isSelectContents])
+  }, [divRefs, isMouseDown, isMouseOutDuringDown, mouseDownRow, mouseOverRow])
 
   return (
     <div
       className="flex flex-col items-center h-full"
-      onMouseDown={divOnMouseDown}
-      onMouseUp={divOnMouseUp}>
+      onMouseDown={onMouseDownPage}
+      onMouseOver={onMouseOverPage}
+      onMouseUp={onMouseUpPage}>
       <Title>DocumentG Page</Title>
       <input
         className="border-2 text-gkd-sakura-text m-2 p-2 text-3xl w-1/3"
@@ -123,22 +122,27 @@ export default function DocumentGPage() {
         {contents &&
           contents.map((content, index) => {
             return (
-              <div ref={el => (divRefs.current[index] = el)}>
+              <div
+                className="w-full selection:bg-gkd-sakura-bg"
+                ref={el => (divRefs.current[index] = el)}>
                 <input
+                  className="w-full border-2 focus:bg-gkd-sakura-bg"
                   key={`contents:${index}`}
-                  onChange={inputOnChange(index)}
-                  onMouseDown={inputOnMouseDown(index)}
-                  onMouseUp={inputOnMouseUp(index)}
-                  onMouseOut={inputOnMouseOut(index)}
-                  onMouseOver={inputOnMouseOver(index)}
+                  onChange={e => {}}
+                  onClick={onClickInput(index)}
+                  onMouseDown={onMouseDownInput(index)}
+                  onMouseOut={onMouseOutInput(index)}
+                  onMouseOver={onMouseOverInput(index)}
+                  onMouseUp={onMouseUpInput(index)}
                   ref={el => (inputRefs.current[index] = el)}
-                  value={content || ''}></input>
+                  value={content || ''}
+                />
               </div>
             )
           })}
-        <input onChange={inputOnChange((contents && contents.length) || 0)} />
+        <input />
       </div>
-      <Button onClick={onClickTestButton}>yes</Button>
+      <Button>yes</Button>
     </div>
   )
 }
